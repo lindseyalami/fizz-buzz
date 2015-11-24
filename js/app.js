fe7d4eacff229c93fe4a5db6fa193ea9f187cf95
lindseyalami <lindseywatt@gmail.com>
2015-11-23 16:33:40 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+  var i;
+
+  for (var i =1; i<101; i++) {
+
+    if(i%15===0) {
+        console.log("fizzbuzz");
+    }
+
+    else if(i%3===0) {
+        console.log("fizz");
+    }
+
+        else if(i%5===0) {
+        console.log("buzz");
+    }
+
+    else {
+        console.log(i);
+    }
+
+});
+
+
+
+
+
